bb9e73e92a57afefe645ba1fad9222a4bfc0688f
Aquib Master <aquibmaster@gmail.com>
2020-09-12 16:59:21 +1200

    Modify indexer to return absolute paths to files

--- src/indexer.ts
+++ src/indexer.ts
@@ -6,5 +6,7 @@ const readdir = promisify(fs.readdir)
 
 export const indexSourceFiles = async (): Promise<string[]> => {
     const sourcePath = path.join(__dirname, '../source-images')
-    return readdir(sourcePath)
+    const files = await readdir(sourcePath)
+
+    return files.map(fileName => path.join(sourcePath, fileName))
 }
