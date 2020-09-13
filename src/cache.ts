2d7848de7542609c705a2fe4404ce87b1b51a746
Aquib Master <aquibmaster@gmail.com>
2020-09-13 15:13:26 +1200

    Implement first pass caching

---
+++ src/cache.ts
@@ -0,0 +1,83 @@
+import path from 'path'
+import fs from 'fs'
+import { promisify } from 'util'
+
+const writeFile = promisify(fs.writeFile)
+const readFile = promisify(fs.readFile)
+const readdir = promisify(fs.readdir)
+
+type CacheMap = {
+    [imageName: string]: {
+        [dimensions: string]: boolean
+    }
+}
+
+export class CacheError extends Error {
+    constructor(message: string) {
+        super(message)
+        this.name = 'CacheError'
+    }
+}
+
+export class Cache {
+    cache: CacheMap
+
+    constructor() {
+        this.cache = {}
+        this.init()
+    }
+
+    async init() {
+        try {
+            const cacheDir = path.join(__dirname, '..', '.cache')
+            const fileNames = await readdir(cacheDir)
+
+            fileNames.forEach(fileName => {
+                const [file, extension] = fileName.split('.')
+                const [name, dimensions] = file.split('___')
+
+                this.cache[name] = {
+                    ...(this.cache[name] || {}),
+                    [dimensions]: true
+                }
+            })
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    async add(imageName: string, width: number, height: number, buffer: Buffer): Promise<void> {
+        try {
+            const [name, extension] = imageName.split('.')
+            const dimensions = `${width}x${height}`
+            const absolutePath = path.join(__dirname, '..', '.cache', `${name}___${dimensions}.${extension}`)
+
+            await writeFile(absolutePath, buffer)
+
+            this.cache[name] = {
+                ...(this.cache[name] || {}),
+                [dimensions]: true
+            }
+        } catch(error) {
+            throw error
+        }
+    }
+
+    async get(imageName: string, width: number, height: number): Promise<Buffer> {
+        try {
+            const [name, extension] = imageName.split('.')
+            const dimensions = `${width}x${height}`
+            const exists = this.cache[imageName] && this.cache[name][dimensions]
+            const absolutePath = path.join(__dirname, '..', '.cache', `${name}___${dimensions}.${extension}`)
+
+            if(!exists) {
+                throw new CacheError('cache-miss')
+            }
+
+            const buffer = await readFile(absolutePath)
+            return buffer
+        } catch(error) {
+            throw error
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as Hapi from '@hapi/hapi'
 import Jimp from 'jimp'
 
-import { indexSourceFiles } from './indexer'
+import { Cache } from './cache'
 import { resize } from './resizer'
+import { indexSourceFiles } from './indexer'
 
 const init = async () => {
     const server = Hapi.server({
@@ -11,8 +12,15 @@ const init = async () => {
     })
 
     const sourceFiles = await indexSourceFiles()
+    const cache = new Cache()
+
     const getRandomSourceFile = (): string =>
         sourceFiles[Math.floor(Math.random() * sourceFiles.length)]
+
+    const getImageName = (imagePath: string): string => {
+        const parts = imagePath.split('\\')
+        return parts[parts.length - 1]
+    }
 
     server.route({
         method: 'GET',
@@ -35,15 +43,24 @@ const init = async () => {
         path: '/{width}/{height}',
         handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
             const imagePath = getRandomSourceFile()
+            const imageName = getImageName(imagePath)
 
             const width = Number(request.params.width) || 480
             const height = Number(request.params.height) || width
 
-            // TODO(AM): Get from cache if available
-            const imageBuffer = await resize(imagePath, width, height)
-            // TODO(AM): Cache the new image
+            try {
 
-            return h.response(imageBuffer).type(Jimp.MIME_JPEG)
+                const imageBuffer = await cache.get(imageName, width, height)
+                return h.response(imageBuffer).type(Jimp.MIME_JPEG)
+
+            } catch(error) {
+
+                if(error.name === 'CacheError') {
+                    const imageBuffer = await resize(imagePath, width, height)
+                    cache.add(imageName, width, height, imageBuffer)
+                    return h.response(imageBuffer).type(Jimp.MIME_JPEG)
+                }
+            }
         }
     })
 
